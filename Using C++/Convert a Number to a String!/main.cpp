f3f66a0c47ae0a84ea19c9807c5c17a66de899f0
Eslam Dawood <dawoodeslam017@gmail.com>
2023-06-28 16:52:37 +0300

    add Problem

---
+++ Using C++/Convert a Number to a String!/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+#include <string>
+using namespace std;
+string number_to_string(int num) {
+  return to_string(num);
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
